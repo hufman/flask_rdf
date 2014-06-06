05ffc9da25154075cd6b325a38281c51ac290d06
Walter Huf <hufman@gmail.com>
2014-06-06 00:13:44 -0500

    Adds packaging info

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+try:
+	from setuptools import setup
+except:
+	pass
+
+requirements = open('requirements.txt').read().split('\n')
+test_requirements = open('requirements.test.txt').read().split('\n')
+
+setup(name='flask_rdf',
+      version='0.1.2',
+      description='Flask decorator to output RDF using content negotiation',
+      author='Walter Huf',
+      url='https://github.com/hufman/flask_rdf',
+      packages=['flask_rdf'],
+      install_requires=requirements,
+      test_requires=requirements + test_requirements
+)
