cfd009ec8da2217ec314d0db4c2d7bb51afdaee2
Walter Huf <hufman@gmail.com>
2014-06-23 23:28:43 -0500

    Adds some extra categories for pypi

--- setup.py
+++ setup.py
@@ -8,13 +8,29 @@ except:
 
 requirements = open('requirements.txt').read().split('\n')
 test_requirements = open('requirements.test.txt').read().split('\n')
+long_description = open('README.md').read()
 
 setup(name='flask_rdf',
       version='0.1.5',
       description='Flask decorator to output RDF using content negotiation',
       author='Walter Huf',
+      author_email='hufman@gmail.com',
       url='https://github.com/hufman/flask_rdf',
       packages=['flask_rdf'],
       install_requires=requirements,
-      test_requires=requirements + test_requirements
+      test_requires=requirements + test_requirements,
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Framework :: Flask',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+     ],
+     license='BSD',
+     platforms=['Any']
 )
