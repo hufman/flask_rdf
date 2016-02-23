9c53e000c4ac930d916bdd19b263e6c4d5eeca07
Walter Huf <hufman@gmail.com>
2016-02-23 12:34:53 -0600

    Adds tox tests and coverage testing

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ try:
 except:
 	pass
 
-requirements = ['Flask', 'python-mimeparse==0.1.4']
-test_requirements = ['nose==1.3.3', 'rdflib']
-
+requirements = open('requirements.txt').read().split('\n')
+test_requirements = open('requirements.test.txt').read().split('\n')
 long_description = open('README.rst').read()
 
 setup(name='flask_rdf',
